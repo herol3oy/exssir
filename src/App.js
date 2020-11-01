5d816dc2bc1eda0f290a192d2c1f403fbb0b40ea
Hamed Sedighi <hamedsedighi@gmail.com>
2020-11-01 14:12:30 +0100

    Style and standardjs lint initialized.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-export default function App() {
+export default function App () {
   return (
     <>
       Hi!
     </>
-  );
+  )
 }
--- src/components/DirectingPage.js
+++ src/components/DirectingPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function DirectingPage() {
+export default function DirectingPage () {
   return (
     <div>
       dpage!
