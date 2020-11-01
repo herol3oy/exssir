50979d173ca2806b43f085fddcba483f1ea68657
Hamed Sedighi <hamedsedighi@gmail.com>
2020-11-01 14:16:14 +0100

    Router initialized.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-export default function App () {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home'
+import DirectingPage from './components/DirectingPage'
+
+export default function App() {
   return (
-    <>
-      Hi!
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/:name' component={DirectingPage} />
+      </Switch>
+    </Router>
   )
 }
