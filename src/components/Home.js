bda93c1c1c1b61f529e0c3df5bdd7f8e78e2e2f4
Hamed Sedighi <hamedsedighi@gmail.com>
2020-11-01 15:29:47 +0100

    Img container created.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
 import Nav from './Nav'
+import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Alert from 'react-bootstrap/Alert'
 
 export default function Home () {
   return (
     <>
       <Nav />
+      <Container>
+        <Row>
+          <Col lg={10}>
+            <Image src='https://source.unsplash.com/1600x900/?art,minimal' fluid />
+            <Alert className='text-right' variant='success'>
+              لطفا برای خرید این اثر هنری بر روی عکس کلیک کنید
+            </Alert>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
