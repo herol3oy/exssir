98c8585a168e094a8add380972315b858dbae04d
Hamed Sedighi <hamedsedighi@gmail.com>
2020-11-01 16:17:05 +0100

    Firebase initialized. Directing page created.

--- src/components/DirectingPage.js
+++ src/components/DirectingPage.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { db, increment } from '../containers/Firebase'
 
-export default function DirectingPage () {
-  return (
-    <div>
-      dpage!
-    </div>
-  )
+export default function DirectingPage ({ match }) {
+  const name = match.params.name
+
+  useEffect(() => {
+    const ref = db.collection('urls')
+
+    ref
+      .where('shortId', '==', name)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach((doc) => {
+          const urlRef = db.collection('urls').doc(doc.id)
+          urlRef.update({ visits: increment })
+          window.location = doc.data().url
+        })
+      })
+      .catch(error => console.log('Error getting documents: ', error))
+  })
+
+  return null
 }
